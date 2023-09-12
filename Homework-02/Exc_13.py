e10082bf477fea59aa7be1a3bf243df1ac78a8a9
Iurii Popov <popov.yk@gmail.com>
2023-09-12 19:26:14 +0300

    ver.3_exc_13

--- Homework-02/Exc_13.py
+++ Homework-02/Exc_13.py
@@ -2,18 +2,24 @@ message = input("Enter a message: ")
 offset = int(input("Enter the offset: "))
 encoded_message = ""
 for ch in message:
-    if ch == ch.upper():
-        pos = ord(ch) - ord('A')
-        pos = (pos - offset) % 26
+    # ch = message[i]
+    # if ch == " ":
+    #     new_ch+= " "
+    # elif ch == "!":
+    #     new_ch+="!"
+    if (ch.isupper()):
+        pos = ord(ch) + ord('A')
+        pos = (pos + offset) % 26
         new_ch = chr(pos + ord('A'))
-    elif ch == ch.islower():
+    elif (ch.islower()):
         pos = ord(ch) - ord('a')
-        pos = (pos - offset) % 26
+        pos = (pos + offset) % 26
         new_ch = chr(pos + ord('a'))
     else:
         new_ch = ch
     encoded_message += new_ch
 print(encoded_message)
+    
 # mv = 1
 # pos=(ord(ch) - ord(ch))
 # print (pos)
@@ -21,3 +27,18 @@ print(encoded_message)
 # print(pos)
 # new_ch = chr(pos + ord("A"))
 # print(ord("A"), ord(new_ch))
+
+
+# for ch in message:
+#     if ch == ch.upper():
+#         pos = ord(ch) - ord('A')
+#         pos = (pos - offset) % 26
+#         new_ch = chr(pos + ord('A'))
+#     elif ch == ch.lower():
+#         pos = ord(ch) - ord('a')
+#         pos = (pos - offset) % 26
+#         new_ch = chr(pos + ord('a'))
+#     else:
+#         new_ch = ch
+#     encoded_message += new_ch
+# print(encoded_message)
