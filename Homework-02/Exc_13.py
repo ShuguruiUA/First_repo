a11be1729c35786786298d19fd3fbec797b9be2c
ShuguruiUA <popov.yk@gmail.com>
2023-09-13 15:41:05 +0300

    Exc_13_v4

--- Homework-02/Exc_13.py
+++ Homework-02/Exc_13.py
@@ -2,12 +2,14 @@ message = input("Enter a message: ")
 offset = int(input("Enter the offset: "))
 encoded_message = ""
 for ch in message:
-    if ch == ch.upper():
+    if ch == " ":
+        new_ch = " "
+    elif ch == ch.upper():
         pos = ord(ch) - ord('A')
-        pos = (pos - offset) % 26
+        pos = (pos + offset) % 26
         new_ch = chr(pos + ord('A'))
-    elif ch == ch.islower():
-        pos = ord(ch) - ord('a')
+    elif ch == ch.lower():
+        pos = ord(ch) + ord('a')
         pos = (pos - offset) % 26
         new_ch = chr(pos + ord('a'))
     else:
