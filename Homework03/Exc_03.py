296929f02d5202b3e898bf6b5e58ccf62868ce46
Iurii Popov <popov.yk@gmail.com>
2023-09-14 21:56:36 +0300

    additional func

--- Homework03/Exc_03.py
+++ Homework03/Exc_03.py
@@ -9,4 +9,10 @@ def calculate_trip_price(distance_km):
     total = distance_km * price_per_km + base_rate
     global total_trip 
     total_trip += 1
+    print(total)
     return total
+calculate_trip_price(12)
+calculate_trip_price(12)
+calculate_trip_price(12)
+calculate_trip_price(12)
+print(total_trip)
