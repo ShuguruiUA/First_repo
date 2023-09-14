1a1d15ce427ab906e0ae265d0ec360a096eb8998
ShuguruiUA <popov.yk@gmail.com>
2023-09-14 16:08:15 +0300

    Exc_15-Final

--- test.py
+++ test.py
@@ -12,29 +12,34 @@ while True:
     elif wait_for_number:
         try:
             operand = int(char) if char.isdigit() else float(char)
-            wait_for_number = False
+
+            if operator == "+":
+                result += operand
+            elif operator == "-":
+                result -= operand
+            elif operator == "*":
+                result *= operand
+            elif operator == '/':
+                try:
+                    result /= operand
+                except ZeroDivisionError:
+                    print(f"Devision by {operand} is not allowed")
+                    continue
+                     
           
         except ValueError:
-            print(f"{operand} is not a number. Try again")
+            print(f"{char} is not a number. Try again")
             continue
+        wait_for_number = False
         if result == None:
            result = operand
 
     else:
-        if char not in ("+", "-", "*", "/"):
+        if char not in ('+', '-', '*', '/'):
             print(f"Char is not a '+' or '-' or '*' or '/'")
             continue
         operator = char
         wait_for_number = True
-        if operator == "+":
-            result += operand
-        elif operator == "-":
-            result -= operand
-        elif operator == "*":
-            result *= operand
-        elif operator == "/":
-            try:
-                if operand == "0":
-                    continue
-            except ZeroDivisionError:
-                print(f"Devision by {operand} is not allowed")
+# Перша: ["10", "+", "5", "6", "/", "3", "-", "a", "2", "*", "6", "= "], результат 18.0
+# Друга: ["2", "3", "-", "1", "+", "10", "*", "2", "="], результат 22.0
+    
