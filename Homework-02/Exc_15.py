357e6e6106e1d614afedf4f6d4b0c154001ba878
ShuguruiUA <popov.yk@gmail.com>
2023-09-14 16:08:34 +0300

    Final

--- Homework-02/Exc_15.py
+++ Homework-02/Exc_15.py
@@ -2,42 +2,43 @@ result = None
 operand = None
 operator = None
 wait_for_number = True
-operator_result = 0
-operand_result = 0
 
 while True:
-    result = 0
-    user_input = input(">>>")
-    
-    if user_input == "=":
-        print(f"result: {result}")
-        break
-    elif user_input != "=":
-        try:
-            result += operator_result
-            operand = user_input
-            operand = int(operand) if operand.isdigit() else float(operand)
-            operand_result=result
-            print("You are in the operand mode")
-        except ValueError:
-            print(f"{operand} is not a number. Try again")
-        
-    else:
-        if user_input == "+":
-            operator_result = operand_result
-            operator_result += operand
-        elif user_input == "-":
-            operator_result = operand_result
-            operator_result -= operand
-        elif user_input == "*":
-            operator_result = operand_result
-            operator_result *= operand
-        elif user_input == "/":
-            try:
-                if operand == "0":
-                    continue
-            except ZeroDivisionError:
-                print(f"Devision by {operand} is not allowed")
-        print("you are in the operator mode")
+    char=input(">>>")
 
-        
+    if char == "=":
+        print(f"Result is {result}")
+        break
+    elif wait_for_number:
+        try:
+            operand = int(char) if char.isdigit() else float(char)
+
+            if operator == "+":
+                result += operand
+            elif operator == "-":
+                result -= operand
+            elif operator == "*":
+                result *= operand
+            elif operator == '/':
+                try:
+                    result /= operand
+                except ZeroDivisionError:
+                    print(f"Devision by {operand} is not allowed")
+                    continue
+                     
+          
+        except ValueError:
+            print(f"{char} is not a number. Try again")
+            continue
+        wait_for_number = False
+        if result == None:
+           result = operand
+
+    else:
+        if char not in ('+', '-', '*', '/'):
+            print(f"Char is not a '+' or '-' or '*' or '/'")
+            continue
+        operator = char
+        wait_for_number = True
+# Перша: ["10", "+", "5", "6", "/", "3", "-", "a", "2", "*", "6", "= "], результат 18.0
+# Друга: ["2", "3", "-", "1", "+", "10", "*", "2", "="], результат 22.0
