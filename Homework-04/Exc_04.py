32afc96c19936c9f2e2a34bb065869ca40cd4fd5
Iurii Popov <popov.yk@gmail.com>
2023-09-27 06:58:36 +0300

    2023-09-27

--- Homework-04/Exc_04.py
+++ Homework-04/Exc_04.py
@@ -4,16 +4,18 @@
 
 def get_grade(key):
     ects_grade = {"F": 1, "FX": 2, "E": 3, "D": 3, "C": 4, "B": 5, "A": 5}
-    if not ects_grade:
-        return f"asd"
+    if key not in ects_grade:
+        return None
     return ects_grade[key]
 
 
 def get_description(key):
     ects_des = {"F": "Unsatisfactorily", "FX": "Unsatisfactorily", "E": "Enough",
                 "D": "Satisfactorily", "C": "Good", "B": "Very good", "A": "Perfectly"}
+    if key not in ects_des:
+        return None
     return ects_des[key]
 
 
 print(get_grade("AA"))
-print(get_description("A"))
+print(get_description("FX"))
