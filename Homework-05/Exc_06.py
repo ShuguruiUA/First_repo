159815ec3a892b0ffbf100bdabf0a71e88549cf7
Iurii Popov <popov.yk@gmail.com>
2023-10-04 22:32:31 +0300

    2023-10-04

--- Homework-05/Exc_06.py
+++ Homework-05/Exc_06.py
@@ -1,7 +1,19 @@
+# def is_spam_words(text, spam_words, space_around=False):
+#     new_text = text.split(" ")
+#     for index in new_text:
+#         if not spam_words:
+#             new_text[index].lower() != spam_words.lower()
+#             index += 1
+#         else:
+#             return True
 def is_spam_words(text, spam_words, space_around=False):
-    for spam_words in text:
-        if not space_around:
-            spam_words = spam_words.lower()
-            text = text.lower()
-        if spam_words in text:
-            
+    text_set = set(text.split())
+    sw_set = set(spam_words)
+    if not space_around:
+        if sw_set.intersection(text_set):
+            return True
+        return False
+
+
+print(is_spam_words('Молох бог ужасен.', ['Лох']))
+print(is_spam_words('Молох бог ужасен.', ['лох'], True))
