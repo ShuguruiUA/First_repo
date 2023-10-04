4fd5dea3216e1bcfe06de6b41ada67b6aa31e4af
ShuguruiUA <popov.yk@gmail.com>
2023-10-04 09:53:15 +0300

    2023-10-04

---
+++ Homework-05/test05.py
@@ -0,0 +1,6 @@
+# for i in range(16):
+#     s = "int: {0:d};  hex: {0:#x};  oct: {0:#o};  bin: {0:#b}".format(i)
+#     print(s)
+width = 5
+for num in range(13):
+    print('{:^10} {:^10} {:^10}'.format(num, num**2, num**3))
