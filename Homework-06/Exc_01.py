55c7657a3c566b1ed931c29f41d5ada1ab264fc0
ShuguruiUA <popov.yk@gmail.com>
2023-10-09 16:09:08 +0300

    2023-10-09

--- Homework-06/Exc_01.py
+++ Homework-06/Exc_01.py
@@ -1,1 +1,15 @@
-fh = open(|test.txt)
+import re
+fh = open('test.txt','r')
+while True:
+    line = fh.readline()
+    sal_sum = 0
+    if not line:
+        break
+    result = re.search(r'\d{1,6}+', line)
+    #print(result)
+    for num in result:
+        for i in num:
+            sal_sum += num[i]
+            i += 1
+    print (sal_sum)
+
