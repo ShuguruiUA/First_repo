1a6054af75acff241fb898aee3f2a2fa181e3bd5
Iurii Popov <popov.yk@gmail.com>
2023-10-09 21:22:57 +0300

    2023-10-09

--- Homework-06/Exc_01.py
+++ Homework-06/Exc_01.py
@@ -1,1 +1,22 @@
-fh = open(|test.txt)
+import re
+
+path = ('test.txt')
+
+
+def total_salary(path):
+    result = 0
+    file = open(path, 'r')
+    while True:
+        line = file.readline()
+        if line:
+            found = re.search(r'\d+', line)
+            print(found, type(found))
+            team_salary = float(int(found.group()))
+            result += team_salary
+        else:
+            break
+    file.close()
+    return result
+
+
+print(total_salary('test.txt'))
