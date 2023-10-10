0ba387c5beee273269c0888e770524a15b070eaf
Iurii Popov <popov.yk@gmail.com>
2023-10-10 21:42:44 +0300

    2023-10-10_ver3

---
+++ Homework-06/Exc_05.py
@@ -0,0 +1,60 @@
+import re
+path = ('bd.txt')
+db_list = list()
+db_dict = {
+    'id': None,
+    'name': None,
+    'age': None
+}
+# with open(path) as file:
+file = open(path, 'r')
+while True:
+    line = file.readline()
+    if line:
+        found_id = re.search(r'\w+{24}', file)
+        print(found_id)
+    file.close()
+# while True:
+#     line = file.readline()
+#     try:
+#         with open(path, 'w+') as file:
+#             while True:
+#                 line = file.readline()
+#                 if line:
+#                     found_id = re.search(r'\d{24}', file)
+#                     found_name = re.search(r'\d{24}([a-zA-z]*?)', file)
+#                     found_age = re.search(
+#                         r'\d+{24},[a-zA-z]*?([0-1]{,2}*)', file)
+#                     db_dict.update({'id': found_id})
+#                     db_dict.update({'name': found_name})
+#                     db_dict.update({'age': found_age})
+#                     db_list.append(db_dict)
+#                 else:
+#                     break
+#         file.close()
+#     except OSError as error:
+#         print(f'Error {error}')
+
+
+# import re
+# def get_cats_info(path):
+#     db_list = list()
+#     db_dict = {
+#         'id': None,
+#         'name': None,
+#         'age': None
+#     }
+#     with open(path, 'w+') as file:
+#         while True:
+#             line = file.readline()
+#             if line:
+#                 found_id = re.search(r'\d{24}', file)
+#                 found_name = re.search(r'\d{24}([a-zA-z]*?)', file)
+#                 found_age = re.search(r'\d+{24},[a-zA-z]*?([0-1]{,2}*)')
+#                 db_dict.update({'id': found_id})
+#                 db_dict.update({'name': found_name})
+#                 db_dict.update({'age': found_age})
+#                 db_list.append(db_dict)
+#             else:
+#                 break
+#         file.close()
---
+++ Homework-06/Test_01.py
@@ -0,0 +1,6 @@
+from pathlib import Path
+current_dir = Path('.')
+current_dir = Path / 'Homework=06'
+print(current_dir.cwd())
+# for element in current_dir.glob('./**/*.txt'):
+#     print(f'{current_dir.cwd()} + {element}')
