05b24f411f3245cda85f122bcb0c020f0b100c2f
ShuguruiUA <popov.yk@gmail.com>
2023-10-11 10:26:39 +0300

    2023-10-11(exc_5) done

--- Homework-06/Exc_05.py
+++ Homework-06/Exc_05.py
@@ -1,75 +1,78 @@
-# import re
+import re
 path = ('bd.txt')
-db_list = list()
-db_dict = {
-    'id': None,
-    'name': None,
-    'age': None
-}
-# with open(path) as file:
-file = open(path, 'r')
-new_list = list()
-line = file.readline()
-while True:
-    line = file.readline().replace('\n', '')
-    if line:
-        line = line.split(',')
-        db_dict.update({'id': line[0]})
-        db_dict.update({'name': line[1]})
-        db_dict.update({'age': line[2]})
+#db_list = list()
+# db_dict = {
+#     'id': None,
+#     'name': None,
+#     'age': None
+# }
+with open(path,'r') as file:
+    db_list = list()
+    for line in file:
+        line.replace('n','')
+        dict_id_search = re.search(r'\w{24}',line).group()
+        dict_name_search = re.search(r'\b[a-zA-Z]+',line).group()
+        dict_age_search = re.search(r'\d+$',line).group()
+
+        print(dict_id_search)
+        print(dict_name_search)
+        print(dict_age_search)
+        db_dict = {
+            'id': dict_id_search,
+            'name' : dict_name_search,
+            'age': dict_age_search
+        }
+        # line.split(',')
+        # print(line)
+        # dict_id_val = line[:24]
+        # print(dict_id_val)
+        # dict_name_val = line
+        # dict_age_val = line[2]
+
+        # db_dict.update({'id':dict_id_search})
+        # db_dict.update({'name':dict_name_search})
+        # db_dict.update({'age':dict_age_search})
         db_list.append(db_dict)
-    else:
-        break
-file.close()
 print(db_list)
 
-# while True:
-#     line = file.readline()
-#     if line:
-#         found_id = re.search(r'\w+{24}', file)
-#         print(found_id)
-#     file.close()
-# while True:
-#     line = file.readline()
-#     try:
-#         with open(path, 'w+') as file:
-#             while True:
-#                 line = file.readline()
-#                 if line:
-#                     found_id = re.search(r'\d{24}', file)
-#                     found_name = re.search(r'\d{24}([a-zA-z]*?)', file)
-#                     found_age = re.search(
-#                         r'\d+{24},[a-zA-z]*?([0-1]{,2}*)', file)
-#                     db_dict.update({'id': found_id})
-#                     db_dict.update({'name': found_name})
-#                     db_dict.update({'age': found_age})
-#                     db_list.append(db_dict)
-#                 else:
-#                     break
-#         file.close()
-#     except OSError as error:
-#         print(f'Error {error}')
+    # line = file.readline().replace('\n','')
+    # line = line.split(',')
+    # print(line)
+    # db_dict.update({'id':line[0]})
+    # db_dict.update({'name':line[1]})
+    # db_dict.update({'age':line[2]})
+    # db_list.append(db_dict)
+    # print(db_dict)
+    # print(db_list)
+    # try:
+    #     while True:
+    #         line = file.readline().replace('\n','')
+    #         line = line.split(',')
+    #         print(line)
 
 
-# import re
-# def get_cats_info(path):
-#     db_list = list()
-#     db_dict = {
-#         'id': None,
-#         'name': None,
-#         'age': None
-#     }
-#     with open(path, 'w+') as file:
-#         while True:
-#             line = file.readline()
-#             if line:
-#                 found_id = re.search(r'\d{24}', file)
-#                 found_name = re.search(r'\d{24}([a-zA-z]*?)', file)
-#                 found_age = re.search(r'\d+{24},[a-zA-z]*?([0-1]{,2}*)')
-#                 db_dict.update({'id': found_id})
-#                 db_dict.update({'name': found_name})
-#                 db_dict.update({'age': found_age})
-#                 db_list.append(db_dict)
-#             else:
-#                 break
-#         file.close()
+    # except:
+    #     print(f'Something wrong')
+    # finally:
+    #     print('we need to close this')
+
+""" how to resolve this """
+"""
+import re
+def get_cats_info(path):
+    with open(path,'r') as file:
+        db_list = list()
+        for line in file:
+            line.replace('n','')
+            dict_id_search = re.search(r'\w{24}',line).group()
+            dict_name_search = re.search(r'\b[a-zA-Z]+',line).group()
+            dict_age_search = re.search(r'\d+$',line).group()
+            db_dict = {
+                'id': dict_id_search,
+                'name' : dict_name_search,
+                'age': dict_age_search
+            }
+            db_list.append(db_dict)
+    print(db_list)
+    return db_list
+""" 
