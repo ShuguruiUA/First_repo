e78f84f4e96c4a7980caca8dc7fb5b895377eeb4
ShuguruiUA <popov.yk@gmail.com>
2023-10-31 11:17:05 +0200

    2023-10-31

--- Homework-09/Exc_03.py
+++ Homework-09/Exc_03.py
@@ -1,29 +1,37 @@
-def caching_fibonacci(x):
-    cache = {
-        'digit': None,
-        'fibonacci': None,
-    }
-    for val in cache:
-        if val not in cache.get('digit'):
-            def fibonacci(n):
-                if n > 0:
-                    if n == 1 or n == 2:
-                        return 1
-                    result = fibonacci(n - 1) + fibonacci(n - 2)
-                    cache.update({'digit': x, 'fibonacci': result})
-                    return f'{result} from fibonacci'
+def caching_fibonacci():
+    cache = {0:0,1:1,2:1}
+    print(cache)
+    def fibonacci(n):
+        if n not in cache:
+            cache[n] = (fibonacci(n -1) + fibonacci(n-2))
+            return cache[n]
+        else:
+            return cache[n]
+    return fibonacci
 
-                else:
-                    return 0
-            return fibonacci
-    return f'{cache[fibonacci]} from cache'
+def main():
+    calc_fibonacci = caching_fibonacci()
+    print(calc_fibonacci(7))
+    print(calc_fibonacci(8))
+    print(calc_fibonacci(9))
+
+if __name__ == '__main__':
+    main()
+       
 
 
 # print(caching_fibonacci(6))
 
-check_cache = caching_fibonacci(13)
-print(check_cache(13))
+# check_cache = caching_fibonacci(13)
+# print(check_cache(13))
 
+# def fibonacci(n):
+#                 if n > 0:
+#                     if n == 1 or n == 2:
+#                         return 1
+#                     result = fibonacci(n - 1) + fibonacci(n - 2)
+#                     cache.update({'digit': x, 'fibonacci': result})
+#                     return f'{result} from fibonacci'
 
 # def out_func(x):
 #     cache = {}
